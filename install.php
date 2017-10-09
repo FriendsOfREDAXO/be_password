dd8fc95bd779a47e419879dc6431a2521994f7a7
Alexander Walther <github.com@alexplus.de>
2017-10-09 22:56:28 +0200

    Sprach-Platzhalter einfügen

--- install.php
+++ install.php
@@ -1,8 +1,8 @@
 <?php
 if(null === rex_config::get('be_password', 'mail_subject_de')){
-    rex_config::set('be_password', 'mail_subject_de', 'Passwort-Reset');
+    rex_config::set('be_password', 'mail_subject_de', $this->i18n('be_password_default_subject'));
 }
 
 if(null === rex_config::get('be_password', 'mail_body_html_de')){
-    rex_config::set('be_password', 'mail_body_html_de', '<p>Um das Paswort neu zu setzen klicken Sie auf diesen Link: <a href="{{url}}">Passwort neu erstellen</a></p>');
+    rex_config::set('be_password', 'mail_body_html_de', '<p>'.$this->i18n('be_password_default_instruction') .'<a href="{{url}}" title="'.$this->i18n('be_password_default_link').'">'.$this->i18n('be_password_default_link') .'</a></p>');
 }
