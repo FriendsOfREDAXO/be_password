5e345a43aa11e54ebdde9d4cc0d6d8ff59fa1a98
schuer <ds@decaf.de>
2017-10-14 19:52:41 +0200

    update forms to use redaxo styles
    uses fragments like on login page

--- views/form.php
+++ views/form.php
@@ -1,36 +1,73 @@
-<form class="has-handler"
-      method="POST"
-      data-handler="submit:BePassHandler:showForm"
->
-    <section class="rex-page-section">
-        <div class="panel panel-default">
-            <header class="panel-heading"><div class="panel-title">Passwort vergessen</div></header>
-            <div class="panel-body">
-                <?php if ('' != $error): ?>
-                    <div class="alert alert-danger"><?php echo $error; ?></div>
-                <?php elseif ('' != $success): ?>
-                    <div class="alert alert-success"><?php echo $success; ?></div>
-                <?php else: ?>
-                    <div class="form-group">
-                        <label for="be_password_email_input">E-Mail-Adresse</label>
-                        <input class="form-control" id="be_password_email_input" type="email" name="email" required/>
-                    </div>
-                <?php endif; ?>
-            </div>
-            <footer class="panel-footer">
-                <div class="rex-form-panel-footer">
-                    <div class="btn-toolbar">
-                        <?php if ('' != $error): ?>
-                            <a class="btn btn-primary" href="">Weiter</a>
-                        <?php elseif ('' != $success): ?>
-                            <a class="btn btn-primary" href="">Weiter</a>
-                        <?php else: ?>
-                            <div class="be_password_submit_link"><a class="" href="">Abbrechen</a></div>
-                            <button type="submit" class="btn btn-primary">Senden</button>
-                        <?php endif; ?>
-                    </div>
-                </div>
-            </footer>
-        </div>
-    </section>
-</form>
+<?php
+if ($success != '') {
+    echo '<div class="rex-js-login-message">' . rex_view::success($success) . "</div>";
+}
+
+if ($error != '') {
+    echo '<div class="rex-js-login-message">' . rex_view::error($error) . "</div>";
+}
+
+if ($success == '') {
+
+    $email = rex_request('email', 'string');
+
+    $content = '';
+    $content .= '<fieldset>';
+
+    $formElements = [];
+
+    $inputGroups = [];
+    $n = [];
+    $n['field'] = '<input class="form-control" type="email" value="' . htmlspecialchars($email) . '" id="be_password_email_input" name="email" />';
+    $n['left'] = '<i class="rex-icon rex-icon-envelope"></i>';
+    $inputGroups[] = $n;
+
+    $fragment = new rex_fragment();
+    $fragment->setVar('elements', $inputGroups, false);
+    $inputGroup = $fragment->parse('core/form/input_group.php');
+
+    $n = [];
+    $n['label'] = '<label for="be_password_email_input">E-Mailadresse:</label>';
+    $n['field'] = $inputGroup;
+    $n['class'] = 'rex-form-group-vertical';
+    $formElements[] = $n;
+
+    $fragment = new rex_fragment();
+    $fragment->setVar('elements', $formElements, false);
+    $content .= $fragment->parse('core/form/form.php');
+
+    $content .= '</fieldset>';
+
+    $formElements = [];
+    $n = [];
+    $n['field'] = '<button class="btn btn-primary" type="submit">Senden</button>';
+    $formElements[] = $n;
+
+    $n = [];
+    $n['field'] = '<a class="btn btn-link be_password_cancel" href="' . rex_url::currentBackendPage() . '">Abbrechen</a>';
+    $formElements[] = $n;
+
+    $fragment = new rex_fragment();
+    $fragment->setVar('elements', $formElements, false);
+    $buttons = $fragment->parse('core/form/submit.php');
+
+    $fragment = new rex_fragment();
+    $fragment->setVar('title', 'Passwort zurücksetzen', false);
+    $fragment->setVar('body', $content, false);
+    $fragment->setVar('buttons', $buttons, false);
+    $content = $fragment->parse('core/page/section.php');
+
+    $content = '
+        <form class="has-handler" data-handler="submit:BePassHandler:showForm" method="post">
+            ' . $content . '
+        </form>
+        <script>
+            (function() {
+                var inputField = $("#be_password_email_input");
+                var initialValue = inputField.val();
+                inputField.val("").val(initialValue).focus(); // focus with trailing caret
+            }());
+        </script>';
+
+    echo $content;
+}
