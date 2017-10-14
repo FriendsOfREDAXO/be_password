97e3326495f8dd71f7250f29dac678229a20ff4c
schuer <ds@decaf.de>
2017-10-14 19:51:56 +0200

    remove help.php

--- help.php
+++
@@ -1,4 +0,0 @@
-<p>Backend-Passwort vergessen?<br/>
-Dieses Adddon sendet dir einen Link um das Passwort neu zu setzen</p>
-<p>Benötigt Addon PHPMailer</p>
-<p>Repository: https://github.com/akuehnis/redaxo5_addon_be_password</p>
