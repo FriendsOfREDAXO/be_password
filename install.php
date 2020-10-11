36fca301e2033e6c769cb31e35fcc200ca7779d1
schuer <ds@decaf.de>
2020-10-11 19:43:06 +0200

    remove obsole install file

--- install.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (null === rex_config::get('be_password', 'mail_subject_de')) {
-    rex_config::set('be_password', 'mail_subject_de', 'Passwort-Reset');
-}
-
-if (null === rex_config::get('be_password', 'mail_body_html_de')) {
-    rex_config::set('be_password', 'mail_body_html_de', '<p>Um das Passwort neu zu setzen, klicken Sie auf diesen Link: <a href="{{url}}">Passwort neu erstellen</a></p>');
-}
