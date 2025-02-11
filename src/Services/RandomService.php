3ea99b745c32cc12ddb37090650f6f0afcb4a9c8
Alexander Walther <mail@alexplus.de>
2025-02-11 01:34:35 +0100

    token generation with `bin2hex(random_bytes(32))`

--- src/Services/RandomService.php
+++ src/Services/RandomService.php
@@ -4,14 +4,8 @@ namespace FriendsOfRedaxo\BePassword\Services;
 
 class RandomService
 {
-    public function createToken(int $length = 100) :string
+    public function createToken() :string
     {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-        }
-        return $randomString;
+        return bin2hex(random_bytes(32));
     }
 }
